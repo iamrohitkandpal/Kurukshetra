c4f5538860b88158cbb4b2337545433253cc00d5
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-06-20 15:45:43 +0530

    Fix bcrypt import in auth middleware to use bcryptjs for compatibility

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const logger = require('../utils/logger');
 const { getIpAddress } = require('../utils/helpers');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 /**
  * Middleware to authenticate JSON Web Token
